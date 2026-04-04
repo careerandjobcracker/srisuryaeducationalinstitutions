b48f0035f03e57f0e191f6a89495ceb5967192cb
careerandjobcracker <saradatenneti.ai@gmail.com>
2026-04-04 15:42:36 +0530

    Updated Events.tsx

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -41,7 +41,7 @@ const Events = () => (
     </div>
 
     <EventVideoSlider videos={liveVideo} title="LIVE - Sri Surya Educational Institutions Annual Day Celebrations 2026" />
-    <EventVideoSlider videos={eventVideos} title="LIVE - Sri Surya Educational Institutions  Events" />
+    <EventVideoSlider videos={eventVideos} title="Sri Surya Educational Institutions  Events" />
 
     <section className="py-20 bg-background">
       <div className="container max-w-4xl">
